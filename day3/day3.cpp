f2eecf46175a4136b9556cc956bb9ff05fe05867
Aimo <daniogdan@gmail.com>
2021-01-15 04:02:58 +0100

    Created serialize part of day3

--- day3/day3.cpp
+++ day3/day3.cpp
@@ -5,6 +5,44 @@
 using namespace std;
 using namespace chrono;
 
-int main() {
+class Node {
+    public:
+        string val;
+        Node* left;
+        Node* right;
+
+        Node(string _val, Node* _left, Node* _right) {
+            val = _val;
+            left = _left;
+            right = _right;
+        }
+};
+
+string serialize(Node* node) {
+    string str = "Node('" + node->val + "'";
+
+    if (node->left != NULL) {
+        str += ", " + serialize(node->left);
+    }
+
+    if (node->right != NULL) {
+        if (node->left == NULL) {
+            str += ",";
+        }
+        str += ", " + serialize(node->right);
+    }
+
+    str += ")";
+
+    return str;
+}
+
+Node* deserialize(string str) {
     
+}
+
+int main() {
+    Node node("test", new Node("test2", NULL, new Node("test4", NULL, NULL)), new Node("test3", NULL, NULL));
+
+    cout << serialize(&node) << "\n";
 }
