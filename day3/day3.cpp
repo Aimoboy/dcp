1277d0dfefbfc636afc9a6378a43ddc3e3e43244
Aimo <daniogdan@gmail.com>
2021-01-16 04:16:15 +0100

    Done with day 3

--- day3/day3.cpp
+++ day3/day3.cpp
@@ -2,6 +2,7 @@
 #include <bits/stdc++.h>
 #include <chrono>
 #include <cstdlib>
+#include <string.h>
 using namespace std;
 using namespace chrono;
 
@@ -37,12 +38,74 @@ string serialize(Node* node) {
     return str;
 }
 
+int find_node_end(string str, int start) {
+    int count = 1;
+    int i = start + 5;
+    while (count != 0) {
+        if (str[i] == '(') {
+            count++;
+        }
+        else if (str[i] == ')') {
+            count--;
+        }
+
+        i++;
+    }
+
+    return i;
+}
+
 Node* deserialize(string str) {
+    int val_start = 6;
+    int chars = 0;
+
+    for (int i = val_start; i < str.length(); i++) {
+        if (str[i] == '\'') {
+            chars = i - val_start;
+            break;
+        }
+    }
+
+    string value = str.substr(val_start, chars);
+
+    if (str[val_start + chars + 1] == ')') {
+        return new Node(value, NULL, NULL);
+    }
+
+    if (str.substr(val_start + chars + 1, 2) == ",,") {
+        int node_start = val_start + chars + 4;
+
+        int node_end = find_node_end(str, node_start);
+
+        Node* right = deserialize(str.substr(node_start, node_end - node_start));
+
+        return new Node(value, NULL, right);
+    }
+
     
+    int node_start = val_start + chars + 3;
+    int node_end = find_node_end(str, node_start);
+
+    Node* left = deserialize(str.substr(node_start, node_end - node_start));
+
+    if (str.length() > node_end + 1 && str[node_end] == ',') {
+        node_start = node_end + 2;
+        node_end = find_node_end(str, node_start);
+
+        Node* right = deserialize(str.substr(node_start, node_end - node_start));
+
+        return new Node(value, left, right);
+    }
+
+    return new Node(value, left, NULL);
 }
 
 int main() {
-    Node node("test", new Node("test2", NULL, new Node("test4", NULL, NULL)), new Node("test3", NULL, NULL));
+    Node* node = new Node("root", new Node("left", new Node("left.left", NULL, NULL), NULL), new Node("right", NULL, NULL));
 
-    cout << serialize(&node) << "\n";
+    cout << serialize(node) << "\n";
+    cout << serialize(deserialize(serialize(node))) << "\n";
+    cout << (serialize(node) == serialize(deserialize(serialize(node)))) << "\n";
+
+    cout << "\n" << (deserialize(serialize(node))->left->left->val == "left.left") << "\n";
 }
